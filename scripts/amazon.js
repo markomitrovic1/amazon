d8fa3bb6c6b8a0f0cd9b02a34cef20e205020998
marko <markomitrovic0987654321@gmail.com>
2024-08-20 12:24:43 +0200

    update  addToCart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -29,7 +29,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -45,7 +45,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart  js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -72,7 +72,7 @@ function updateCartQuantity() {
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
+    const { productId } = button.dataset;
     addToCart(productId);
     updateCartQuantity();
   });
