41cbe2cd1c9f9b61c37e8520e32ad054379b200b
marko <markomitrovic0987654321@gmail.com>
2024-08-21 13:44:21 +0200

    updateCartQuantity

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -108,5 +108,19 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
       `.js-cart-item-container-${productId}`
     );
     container.remove();
+
+    updateCartQuantity();
   });
 });
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(".js-return-to-home-link").innerHTML = cartQuantity;
+}
+
+updateCartQuantity();
