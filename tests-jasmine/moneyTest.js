a8284ad18dbc25bbf581dd81b513c0b2178b5d6e
marko <markomitrovic0987654321@gmail.com>
2024-08-25 18:41:52 +0200

    add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+  it("convert cents into dollars", () => {
+    expect(formatCurrency(1095)).toEqual("10.95");
+  });
+
+  it("works with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("rounds up to the nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+});
